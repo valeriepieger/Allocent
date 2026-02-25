2bdfdebbfc0a1f18ebf04106880045371828d6eb
megandomingo <megankdomingo@gmail.com>
2026-02-25 14:33:14 -0600

    dashboard and expense

---
+++ BudgetApp/Models and Data Structures/BudgetModels.swift
@@ -0,0 +1,29 @@
+import Foundation
+
+struct BudgetCategory: Identifiable, Codable, Hashable {
+    var id: String
+    var name: String
+    var limit: Double
+    var colorHex: String?
+}
+
+struct Expense: Identifiable, Codable, Hashable {
+    var id: String
+    var categoryId: String
+    var amount: Double
+    var date: Date
+    var note: String?
+    var month: String
+}
+
+struct CategorySummary: Identifiable, Hashable {
+    var id: String
+    var name: String
+    var limit: Double
+    var spent: Double
+    
+    var left: Double {
+        max(limit - spent, 0)
+    }
+}
+
